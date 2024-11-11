dff10f29973b6a61c8f3ddf83b4cf98f70c82b85
Adriel Amadi <adrielamadi7@gmail.com>
2024-11-11 14:47:31 +0100

    updated the frontend origin again

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://task-manager-client-dmuv.onrender.com/');
+    res.header('Access-Control-Allow-Origin', 'https://task-manager-client-dmuv.onrender.com');
     res.header('Access-Control-Allow-Methods', 'GET,POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
